2b0e5713685b17aa0f6d7217d1f0246e1f07a018
Giridhar Chettiar <giri.chettiar@gmail.com>
2024-03-08 18:06:58 +1030

    Coomiting a test change for develop branch : deployment purposes

--- crest-nextapp/components/hero-home.tsx
+++ crest-nextapp/components/hero-home.tsx
@@ -18,7 +18,7 @@ export default function HeroHome() {
             {/* Content */}
             <div className="text-center md:text-left md:min-w-[30rem]" data-aos="fade-right">
               <h1 className="h1 font-playfair-display text-slate-100 mb-4">Create stunning web experiences</h1>
-              <p className="text-xl text-slate-400 mb-8">Our landing page template works on all devices, so you only have to set it up once, and get beautiful results forever.</p>
+              <p className="text-xl text-slate-400 mb-8">Our MCI landing page template works on all devices, so you only have to set it up once, and get beautiful results forever.</p>
               <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center md:justify-start space-y-4 sm:space-y-0 sm:space-x-4">
                 <div>
                   <Link className="btn text-white bg-blue-600 hover:bg-blue-700 w-full group" href="/request-demo">
