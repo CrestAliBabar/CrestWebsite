1a538ebea9238a6cfa54413c846397e58870020b
Giridhar Chettiar <giri.chettiar@gmail.com>
2024-03-08 17:15:02 +1030

    reverting next.js changes

--- crest-nextapp/next.config.js
+++ crest-nextapp/next.config.js
@@ -1,8 +1,15 @@
-//const { withContentlayer } = require("next-contentlayer");
+const { withContentlayer } = require("next-contentlayer");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-};
-module.exports = {
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 
-}
+};
+
+module.exports = withContentlayer(nextConfig);
