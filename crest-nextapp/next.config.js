2095b877ef9a7f0161e2d7a93a11d24661072497
Giridhar Chettiar <giri.chettiar@gmail.com>
2024-03-08 17:46:34 +1030

    reverting again

--- crest-nextapp/next.config.js
+++ crest-nextapp/next.config.js
@@ -1,15 +1,10 @@
-const { withContentlayer } = require("next-contentlayer");
+//const { withContentlayer } = require("next-contentlayer");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
 
 };
 
-module.exports = withContentlayer(nextConfig);
+module.exports = {
+
+  }
