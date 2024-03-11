8878e9dcf503eaece890fb5c76f3a6551e9190c0
Yaokun <yiukuenchu@gmail.com>
2024-03-11 10:52:29 +1030

    fix: fix generateStaticParams() return type error by using mock posts data

--- crest-nextapp/app/(default)/blog/[slug]/page.tsx
+++ crest-nextapp/app/(default)/blog/[slug]/page.tsx
@@ -6,13 +6,25 @@ import PostDate from "@/components/post-date";
 import Separator from "@/components/separator";
 import Newsletter from "@/components/newsletter";
 
-export async function generateStaticParams() {}
+const allPosts = [{ slug: "post-1" }, { slug: "post-2" }];
+
+export async function generateStaticParams() {
+  return allPosts.map((post) => ({
+    slug: post.slug,
+  }));
+}
 
 export async function generateMetadata({
   params,
 }: {
   params: { slug: string };
-}) {}
+}): Promise<Metadata | undefined> {
+  const post = allPosts;
+
+  if (!post) return;
+
+  return {};
+}
 
 export default async function SinglePost({
   params,
