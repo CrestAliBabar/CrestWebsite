8b41d17f530702500e5d8df1c1b6f779d412c76d
Kylektt <kylektt@outlook.com>
2024-03-15 12:51:48 +1030

    completed card component

--- crest-nextapp/app/(default)/(navPages)/Consulting/page.tsx
+++ crest-nextapp/app/(default)/(navPages)/Consulting/page.tsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import CommonPageComponent from "@/app/component/commonPageComponent";
 
 const Consulting = () => {
+
+const dummyData = [
+  {
+    title: "Short to mid-term consulting",
+    subtitle: "Subtitle for the first section",
+    description:
+      "Use our expertise to solve your business challenges! For companies from all industries who want to use the power of open source (or need to defend against it).",
+    imageUrl: "https://i0.wp.com/bayave.com/wp-content/uploads/2023/05/Image-Consulting.png?w=1728&ssl=1",
+  },
+  {
+    title: "Long-term advisory services",
+    subtitle: "Subtitle for the second section",
+    description:
+      "We provide long-term advisory services for commercial open source firms and everyone who uses open source as a strategy to achieve business goals.",
+    imageUrl: "https://i0.wp.com/bayave.com/wp-content/uploads/2023/05/Image-Newsletter.png?w=1728&ssl=1",
+  },
+  // Add more data as needed
+];
+const pageInfo = {
+  heading:"Consulting and Advisory",
+  description:"Consulting and advisory services on open source, inner source, and product strategy."
+}
   return (
-    <div>Consulting</div>
+    <CommonPageComponent componentData={dummyData}  pageInfo={pageInfo} />
   )
 }
 
--- crest-nextapp/app/component/commonPageComponent.tsx
+++ crest-nextapp/app/component/commonPageComponent.tsx
@@ -1,34 +1,47 @@
-import React from 'react';
-import Image from 'next/image';
+import React from "react";
+import Image from "next/image";
+
+
+type PageInfoType = {
+  heading: string;
+  description: string;
+};
 
 type CommonComponentTypeProps = {
   componentData: any[];
+  pageInfo: PageInfoType
 };
 
-const CommonPageComponent: React.FC<CommonComponentTypeProps> = ({ componentData }) => {
+
+const CommonPageComponent: React.FC<CommonComponentTypeProps> = ({
+  componentData,
+  pageInfo
+}) => {
   return (
     <>
+      <div className="container mx-auto mt-20 mb-10">
+        <h1 className="text-3xl font-bold my-4">{pageInfo.heading}</h1>
+        <p className="text-xl">
+          This is a description for the training page.
+        </p>
+      </div>
       {componentData.map((data, index) => (
         <section key={index} className="text-gray-600 body-font">
-          <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-            <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <div className="container mx-auto flex px-5 py-5 mb-10 md:flex-row flex-col items-center bg-gray-300">
+            <div className="lg:max-w-lg md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left md:mb-0 items-center text-center">
               <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
                 {data.title}
-                <br className="hidden lg:inline-block" />
-                {data.subtitle}
               </h1>
               <p className="mb-8 leading-relaxed">{data.description}</p>
-              <div className="flex justify-center">
-                <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-                  Button
-                </button>
-                <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
-                  Button
-                </button>
-              </div>
             </div>
-            <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-              <Image className="object-cover object-center rounded" height={600} width={720} alt="hero" src="https://dummyimage.com/720x600" />
+            <div className="lg:flex-grow md:w-1/2 w-5/6  bg-white p-2">
+              <div className=" w-full md:h-48 lg:h-80 ">
+                <img
+                  src={data.imageUrl}
+                  alt="description"
+                  className=" w-full h-full object-cover"
+                />
+              </div>
             </div>
           </div>
         </section>
