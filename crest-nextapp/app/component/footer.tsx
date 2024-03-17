e952e2cc0580df8fef2005d2b1970512b76fa0b6
Kylektt <kylektt@outlook.com>
2024-03-17 15:51:16 +1030

    updated footer

--- crest-nextapp/app/component/footer.tsx
+++ crest-nextapp/app/component/footer.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <footer className="text-gray-600 body-font">
+    <footer className="text-gray-600 body-font bg-purple-800">
       <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
         <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-          <svg
+          {/* <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             stroke="currentColor"
@@ -16,22 +16,24 @@ export const Footer = () => {
             viewBox="0 0 24 24"
           >
             <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-          </svg>
-          <span className="ml-3 text-xl">Tailblocks</span>
+          </svg> */}
+          <img src="https://crest-centre.net/static/aeeed2f0ba60d911b788182a7cdf32f5/99a5f/dark-logo.webp" alt="" />
+          {/* https://crest-centre.net/static/aeeed2f0ba60d911b788182a7cdf32f5/99a5f/dark-logo.webp */}
+          {/* <span className="ml-3 text-xl">CREST</span> */}
         </a>
-        <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
-          © 2020 Tailblocks —
+        <p className="text-sm text-white sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
+          © 2024 CREST —
           <a
-            href="https://twitter.com/knyttneve"
-            className="text-gray-600 ml-1"
+            // href="https://twitter.com/knyttneve"
+            className="text-white ml-1"
             rel="noopener noreferrer"
             target="_blank"
           >
-            @knyttneve
+            @CREST
           </a>
         </p>
         <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-          <a className="text-gray-500">
+          <a className="text-white">
             <svg
               fill="currentColor"
               strokeLinecap="round"
@@ -43,7 +45,7 @@ export const Footer = () => {
               <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
             </svg>
           </a>
-          <a className="ml-3 text-gray-500">
+          <a className="ml-3 text-white">
             <svg
               fill="currentColor"
               strokeLinecap="round"
@@ -55,7 +57,7 @@ export const Footer = () => {
               <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
             </svg>
           </a>
-          <a className="ml-3 text-gray-500">
+          <a className="ml-3 text-white">
             <svg
               fill="none"
               stroke="currentColor"
@@ -69,7 +71,7 @@ export const Footer = () => {
               <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
             </svg>
           </a>
-          <a className="ml-3 text-gray-500">
+          <a className="ml-3 text-white">
             <svg
               fill="currentColor"
               stroke="currentColor"
