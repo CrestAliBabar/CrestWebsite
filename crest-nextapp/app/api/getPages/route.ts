9a7e4ee9cf6f1af27a51435eae2abd2397c2c316
Giridhar Chettiar <giri.chettiar@gmail.com>
2024-05-13 20:03:28 +0930

    Commting changes for API key integration using environmental variables

--- crest-nextapp/app/api/getPages/route.ts
+++ crest-nextapp/app/api/getPages/route.ts
@@ -1,32 +1,29 @@
-import { getPageDetailForBotPress } from '@/sanity/sanity-utils';
-import { NextResponse } from 'next/server';
-import { type NextRequest } from 'next/server'
+import { getPageDetailForBotPress } from "@/sanity/sanity-utils";
+import { NextResponse } from "next/server";
+import { type NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
-  if (request.method === 'GET') {
-    
+  if (request.method === "GET") {
     const requestHeaders = new Headers(request.headers);
-
-    const apiKey = request.headers.get("authorization")?.split(' ')[1];
-    if (apiKey !== "test") { 
-     return NextResponse.json({ message: 'Invalid API key' }, { status: 401 });
+    const API_Key = process.env.API_key;
+    const apiKey = request.headers.get("authorization")?.split(" ")[1];
+    if (apiKey !== API_Key) {
+      return NextResponse.json({ message: "Invalid API key" }, { status: 401 });
     }
 
     const parameter = request.nextUrl.searchParams.get("page");
-    // console.log(parameter)
-    const pageDetails = await getPageDetailForBotPress(parameter?.toString())
-    
-    // console.log(pageDetails[0])
-
+    const pageDetails = await getPageDetailForBotPress(parameter?.toString());
 
     const domainName = "http://" + request.headers.get("host");
 
     // Construct the response array in the desired format
-    const formattedPages = pageDetails[0].pages.map((page: { text: any; _type: any; _key: any; }) => ({
-      PageTitle: page.text,
-      url: `${domainName}/${parameter}/${page._type}/${page._key}`
-    }));
+    const formattedPages = pageDetails[0].pages.map(
+      (page: { text: any; _type: any; _key: any }) => ({
+        PageTitle: page.text,
+        url: `${domainName}/${parameter}/${page._type}/${page._key}`,
+      })
+    );
 
-    return NextResponse.json({ pages: formattedPages },{status:200,});
+    return NextResponse.json({ pages: formattedPages }, { status: 200 });
   }
 }
