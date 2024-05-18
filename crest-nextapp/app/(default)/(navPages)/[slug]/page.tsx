53fb41cb39bdc355350e08e3e8b5308a135e0f32
Yaokun <yiukuenchu@gmail.com>
2024-05-18 22:06:36 +0930

    feat: implement 404-page-not-found

--- crest-nextapp/app/(default)/(navPages)/[slug]/page.tsx
+++ crest-nextapp/app/(default)/(navPages)/[slug]/page.tsx
@@ -1,3 +1,5 @@
+import { getNavigationTitle } from "@/sanity/sanity-utils";
+import { notFound } from "next/navigation";
 import React from "react";
 
 interface CardsProps {
@@ -7,5 +9,10 @@ interface CardsProps {
 }
 
 export default async function Cards({ params: { slug } }: CardsProps) {
+  const titles = await getNavigationTitle();
+  const isFound = titles.find((title) => title.slug.current === slug);
+  if (!isFound) {
+    notFound();
+  }
   return <>{slug}</>;
 }
