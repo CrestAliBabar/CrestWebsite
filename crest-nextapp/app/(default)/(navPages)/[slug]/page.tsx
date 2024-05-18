76e9896127453949396a471df3802efdef9df4b0
Kylektt <kylektt@outlook.com>
2024-05-18 21:13:47 +0930

    completed nav page component

--- crest-nextapp/app/(default)/(navPages)/[slug]/page.tsx
+++ crest-nextapp/app/(default)/(navPages)/[slug]/page.tsx
@@ -1,11 +1,20 @@
 import React from "react";
+import {getNavigationPageContent} from "@/sanity/sanity-utils";
+import ContentComponent from "@/app/component/contentComponent/contentComponent";
 
-interface CardsProps {
+
+interface PageContentProps {
   params: {
     slug: string;
   };
 }
 
-export default async function Cards({ params: { slug } }: CardsProps) {
-  return <>{slug}</>;
+export default async function PageContentForNavPage({
+  params: { slug },
+}: PageContentProps) {
+  const pageContent = await getNavigationPageContent(slug);
+  const pageBuilder = pageContent[0].navigationPages[0].pageBuilder;
+  // console.log(pageContent)
+  // console.log("pageBuilder: ", pageBuilder)
+  return <ContentComponent Contents={pageBuilder} />;
 }
