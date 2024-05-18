a783a9d80b7ef263b2f60a208b97c448700be7b9
Giridhar Chettiar <giri.chettiar@gmail.com>
2024-05-18 19:14:08 +0930

    unisntalled and removed client

--- crest-nextapp/app/(default)/layout.tsx
+++ crest-nextapp/app/(default)/layout.tsx
@@ -1,5 +1,3 @@
-"use client"
-import { useEffect } from 'react'
 import Header from "../component/header";
 import Footer from "../component/footer";
 import { getLayoutSettings } from "@/sanity/sanity-utils";
