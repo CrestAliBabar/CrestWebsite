284f572b6632c58616b9ea857951b68366286a8d
Yaokun <yiukuenchu@gmail.com>
2024-05-27 21:33:27 +0930

    fix: restyle the video component to make it center-aligned and responsive

--- crest-nextapp/app/component/contentComponent/videoComponent.tsx
+++ crest-nextapp/app/component/contentComponent/videoComponent.tsx
@@ -1,26 +1,24 @@
-import React from "react";
+import React from 'react';
 
 type VideoProps = {
   videoContent: any;
 };
 
 const VideoComponent: React.FC<VideoProps> = ({ videoContent }) => {
-  const videoId = videoContent.url.split("watch?v=")[1].split("&")[0];
+  const videoId = videoContent.url.split('watch?v=')[1].split('&')[0];
   const embedUrl = `https://www.youtube.com/embed/${videoId}`;
   return (
-    <div
-      key={videoContent._key}
-      className="aspect-w-16 aspect-h-9 mb-6 ml-40 mr-40"
-    >
-      <iframe
-        width="660"
-        height="415"
-        loading="lazy"
-        src={embedUrl}
-        title="YouTube video player"
-        allowFullScreen
-      ></iframe>
-      <p>{videoContent.text}</p>
+    <div className='flex flex-col items-center w-full mb-6 px-20 sm:px-9 md:px-16 lg:px-32 xl:px-64 2xl:px-96'>
+      <div key={videoContent._key} className='relative w-full pb-[56.25%] mb-6'>
+        <iframe
+          className='absolute top-0 left-0 w-full h-full'
+          loading='lazy'
+          src={embedUrl}
+          title='YouTube video player'
+          allowFullScreen
+        ></iframe>
+        <p className='text-center'>{videoContent.text}</p>
+      </div>
     </div>
   );
 };
