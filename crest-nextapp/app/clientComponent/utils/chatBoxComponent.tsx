6173e9a56f3982c09bb511dc9a85e3918dc67ca3
Giridhar Chettiar <giri.chettiar@gmail.com>
2024-06-04 12:07:05 +0930

    Chatbox trigger for calendly

--- crest-nextapp/app/clientComponent/utils/chatBoxComponent.tsx
+++ crest-nextapp/app/clientComponent/utils/chatBoxComponent.tsx
@@ -1,15 +1,47 @@
 "use client";
 import Script from "next/script";
-import React from "react";
+import React, { useEffect } from "react";
+
+// Extend the Window interface to include botpressWebChat
+declare global {
+  interface Window {
+    botpressWebChat: any;
+    Calendly: any;
+  }
+}
 
 const ChatBoxComponent = () => {
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (window.botpressWebChat && window.Calendly) {
+        window.botpressWebChat.onEvent(
+          function (event: { value: any; type: string }) {
+            if (event.type === "TRIGGER") {
+              if (event.value && event.value.action === "showCalendly")
+                window.Calendly.showPopupWidget(
+                  "https://calendly.com/crestalibabar?hide_gdpr_banner=1"
+                );
+            }
+          },
+          ["TRIGGER"]
+        );
+        clearInterval(interval);
+      }
+    }, 100);
+  }, []);
+
   return (
     <>
-      <Script src="https://cdn.botpress.cloud/webchat/v1/inject.js"></Script>
+      <Script
+        src="https://cdn.botpress.cloud/webchat/v1/inject.js"/>
       <Script
         src="https://mediafiles.botpress.cloud/285b8507-8ccd-4a2b-b351-b7ea9a9cc39a/webchat/config.js"
-        defer
-      ></Script>
+
+      />
+      <Script
+        src="https://assets.calendly.com/assets/external/widget.js"
+        type="text/javascript" async
+      />
     </>
   );
 };
